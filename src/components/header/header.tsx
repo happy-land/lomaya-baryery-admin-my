6b7c5fb51037a2b2512289082bf5867750d5912f
Ruslan Kulish <ruslan.s.kulish@gmail.com>
2022-11-26 11:06:24 +0300

    header styling done

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,16 +1,23 @@
+import { UserIcon } from '../../ui/icons';
 import { EnterIcon } from '../../ui/icons/enter-icon';
+import { Logo } from '../../ui/logo/logo';
+import '../../assets/styles/common.css';
 import styles from './header.module.css';
 
 export const Header = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.logoContainer}>Лого</div>
+      <div className={styles.logoContainer}>
+        <Logo />
+        <p className={`${styles.logo__title} text_type_main-large`}>ломая барьеры</p>
+      </div>
       <div className={styles.links}>
         <button type='button' className={`${styles.button} text_type_main-medium`}>
+          <UserIcon type='link' />
           Аккаунт
         </button>
         <button type='button' className={`${styles.button} text_type_main-medium`}>
-          <EnterIcon type='link'/>
+          <EnterIcon type='link' />
           Выйти
         </button>
       </div>
---
+++ src/ui/logo/logo.tsx
@@ -0,0 +1,9 @@
+export const Logo = ({ ...props }) => {
+  return (
+    <svg width={40} height={39} fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
+      <path d='M0 31.884V2.32h40L0 31.884Z' fill='#4154A4' />
+      <path d='M0 29.565V0h40L0 29.565ZM40 16.232v21.836H0l40-21.836Z' fill='#3EA745' />
+      <path d='M40 18.164V40H0l40-21.836Z' fill='#4154A4' />
+    </svg>
+  );
+};
